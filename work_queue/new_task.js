e4fb3e18da155cb2fcd732b03a32c3a5d020133a
ValoraSheikh <haarishsheikh04@gmail.com>
2026-06-18 15:00:02 +0530

    change folder

---
+++ work_queue/new_task.js
@@ -0,0 +1,22 @@
+import { connect } from "amqplib";
+
+async function newTask() {
+  const connection = await connect("amqp://localhost:5672");
+  const channel = await connection.createChannel();
+
+  const queue = "task_queue";
+  const message = process.argv.slice(2).join(' ') || "Hello World!";
+
+  await channel.assertQueue(queue, {
+    durable: true,
+    arguments: {
+      'x-queue-type': 'quorum'
+    }
+  });
+  
+  channel.sendToQueue(queue, Buffer.from(message), { persistent: true });
+
+  console.log(` [x] Sent ${message}`);
+}
+
+newTask();
